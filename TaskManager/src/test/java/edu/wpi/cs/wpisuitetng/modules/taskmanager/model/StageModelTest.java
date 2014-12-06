56d0c5c543d4f080fd187949aad0e4856fc588bd
alec12094 <abenson@wpi.edu>
2014-12-06 03:33:58 -0500

    added some tests

--- TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/StageModelTest.java
+++ TaskManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/model/StageModelTest.java
@@ -2,6 +2,8 @@ package edu.wpi.cs.wpisuitetng.modules.taskmanager.model;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.task.TaskModel;
@@ -12,13 +14,13 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.task.TaskModel;
  */
 public class StageModelTest {
 
-	StageModel stage = new StageModel();
-	TaskModel task = new TaskModel();
 	/**
 	 * Test all methods in StageModel
 	 */
 	@Test
 	public void stageTest() {
+		StageModel stage = new StageModel();
+		TaskModel task = new TaskModel();
 		stage.setTitle("Stage");
 		stage.setID(2);
 		stage.setIsArchived(true);
@@ -32,6 +34,49 @@ public class StageModelTest {
 		assertFalse(stage.equals(task));
 		assertFalse(stage.equals(null));
 		assertTrue(stage.getIsArchived());
+	}
+	
+	
+	@Test
+	public void addTaskModel() {
+		StageModel stage = new StageModel();
+		HashMap<Integer, TaskModel> hashMap = new HashMap<Integer, TaskModel>();
+		TaskModel task = new TaskModel();
+		TaskModel task2 = new TaskModel();
+		TaskModel task3 = new TaskModel();
+		
+		hashMap.put(task.getID(), task);
+		hashMap.put(task2.getID(), task2);
+		hashMap.put(task3.getID(), task3);
+		stage.addTaskModel(task);
+		stage.addTaskModel(task2);
+		stage.addTaskModel(task3);
+		
+		assertEquals(hashMap, stage.getTaskModelList());
+		
+	}
+	
+	@Test
+	public void removeTask() {
+		StageModel stage = new StageModel();
+		HashMap<Integer, TaskModel> hashMap = new HashMap<Integer, TaskModel>();
+		TaskModel task = new TaskModel();
+		TaskModel task2 = new TaskModel();
+		TaskModel task3 = new TaskModel();
+		
+		hashMap.put(task.getID(), task);
+		hashMap.put(task2.getID(), task2);
+		hashMap.put(task3.getID(), task3);
+		stage.addTaskModel(task);
+		stage.addTaskModel(task2);
+		stage.addTaskModel(task3);
+		
+		assertEquals(hashMap, stage.getTaskModelList());
+		
+		stage.removeTask(task2);
+		hashMap.remove(task2.getID());
+		
+		assertEquals(hashMap, stage.getTaskModelList());
 	}
 
 }
