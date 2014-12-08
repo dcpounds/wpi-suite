c4c8306956cefea3c3939ca1ced4686de2e7f11c
Joe Gallagher <joedgallagher@wpi.edu>
2014-12-08 12:09:08 -0500

    implementation of task comparison messages for a few change options

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/reports/DataLoggerModel.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/reports/DataLoggerModel.java
@@ -1,6 +1,10 @@
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.reports;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 
 /**
  * @author Joe
@@ -24,8 +28,8 @@ public class DataLoggerModel {
 		return taskSnapID;
 	}
 	
-	/** Returns the most recent 
-	 * @param updatedTask the stage to copy from
+	/** Returns the most recent snapshot associated with the task passed as an argument
+	 * @param taskSnapshot the snapshot to find the most recent previous match for
 	 */
 	public TaskSnapshot returnPreviousSnapshot(TaskSnapshot taskSnapshot) {
 		//checks each task for an ID match, starting from the most recent tasks
@@ -38,6 +42,63 @@ public class DataLoggerModel {
 		}
 		//executed only if the for loop reaches the end, indicating that only one snapshot has been taken of the task
 		return null;
+	}
+	
+	/** Returns a string array of changes made between two snapshots
+	 * @param snap1, the first (older) snapshot
+	 * @param snap2, the second (newer) snapshot
+	 */
+	public int trackChanges(TaskSnapshot snap1, TaskSnapshot snap2) {
+		int changes = 0;
+		
+		List<String> changelog = new LinkedList<String>();
+		
+
+				
+		if (snap1.getActualEffort()!=snap2.getActualEffort())
+		{
+			changelog.add("Changed actual effort from " + snap1.getActualEffort() + " to " + snap2.getActualEffort()
+							+ ".");
+		}
+		if (!(snap1.getCatColor().equals(snap2.getCatColor())))
+		{
+			changelog.add("Changed the category from " + snap1.colorToString(snap1.getCatColor()) + " to " + 
+							snap2.colorToString(snap2.getCatColor()) + ".");
+		}
+		if (!(snap1.getColor().equals(snap2.getColor())))
+		{
+			changes++;
+		}
+		if (!(snap1.getCreationDate().equals(snap2.getCreationDate())))
+		{
+			changes++;
+		}
+		if (!(snap1.getDescription().equals(snap2.getDescription())))
+		{
+			changes++;
+		}
+		if (!(snap1.getDueDate().equals(snap2.getDueDate())))
+		{
+			
+			changes++;
+		}
+		if (snap1.getEstimatedEffort()!=snap2.getEstimatedEffort())
+		{
+			changes++;
+		}
+		if(snap1.getStageID()!=snap2.getStageID())
+		{
+			changes++;
+		}
+		if(!(snap1.getTitle().equals(snap2.getTitle())))
+		{
+			changes++;
+		}
+		
+		
+
+		return changes;
+		
 	}
 	
 	
--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/reports/TaskSnapshot.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/reports/TaskSnapshot.java
@@ -3,6 +3,7 @@ package edu.wpi.cs.wpisuitetng.modules.taskmanager.reports;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Hashtable;
 
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.controller.WorkflowController;
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.model.task.ActivityListModel;
@@ -28,6 +29,14 @@ public class TaskSnapshot {
 	private Color color;
 	private Color CatColor;
 	private ActivityListModel activities;
+	
+	static Hashtable <Color, String> colors;
+	
+	
+
+	
+	
+	
 	/** The default constructor for a Task Snapshot **/
 	public TaskSnapshot(TaskModel task, int id)
 	{
@@ -48,9 +57,229 @@ public class TaskSnapshot {
 		this.color = task.getColor();
 		this.CatColor = task.getCatColor();
 	}
+	
+	
+	//reverse of hashtable used for setting colors, used to drive string creation
+	private Hashtable addColors(){
+		colors = new<String, Color> Hashtable();
+		colors.put(Color.LIGHT_GRAY, "GRAY");
+		colors.put(Color.WHITE, "WHITE");
+		colors.put(new Color(0xA67C52), "BROWN");
+		colors.put(new Color(0xF7977A), "RED");
+		colors.put(new Color(0xF49AC2), "PINK");
+		colors.put(new Color(0xFDC68A), "ORANGE");
+		colors.put(new Color(0xFFF79A), "YELLOW");
+		colors.put(new Color(0x82CA9D), "GREEN");
+		colors.put(new Color(0x8493CA), "BLUE");
+		colors.put(new Color(0xA187BE), "PURPLE");
+		
+		
+		return colors;
+	}
+	
+	public String colorToString(Color color)
+	{
+		addColors();
+		return colors.get(color);
+	}
+	
+	
+	
+	public void setID(long id)
+	{
+		this.id=id;
+	}
+	
+	public long getID()
+	{
+		return id;
+	}
+	
+	public void setTaskID(int id)
+	{
+		this.taskID = id;
+	}
+	
 	public int getTaskID()
 	{
 		return taskID;
 	}
+	/**
+	 * @return the estimatedEffort
+	 */
+	public int getEstimatedEffort() {
+		return estimatedEffort;
+	}
+	/**
+	 * @param estimatedEffort the estimatedEffort to set
+	 */
+	public void setEstimatedEffort(int estimatedEffort) {
+		this.estimatedEffort = estimatedEffort;
+	}
+	/**
+	 * @return the actualEffort
+	 */
+	public int getActualEffort() {
+		return actualEffort;
+	}
+	/**
+	 * @param actualEffort the actualEffort to set
+	 */
+	public void setActualEffort(int actualEffort) {
+		this.actualEffort = actualEffort;
+	}
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the creatorName
+	 */
+	public String getCreatorName() {
+		return creatorName;
+	}
+	/**
+	 * @param creatorName the creatorName to set
+	 */
+	public void setCreatorName(String creatorName) {
+		this.creatorName = creatorName;
+	}
+	/**
+	 * @return the usersAssignedTo
+	 */
+	public ArrayList<String> getUsersAssignedTo() {
+		return usersAssignedTo;
+	}
+	/**
+	 * @param usersAssignedTo the usersAssignedTo to set
+	 */
+	public void setUsersAssignedTo(ArrayList<String> usersAssignedTo) {
+		this.usersAssignedTo = usersAssignedTo;
+	}
+	/**
+	 * @return the creationDate
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	/**
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	/**
+	 * @return the dueDate
+	 */
+	public String getDueDate() {
+		return dueDate;
+	}
+	/**
+	 * @param dueDate the dueDate to set
+	 */
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	/**
+	 * @return the stageID
+	 */
+	public int getStageID() {
+		return stageID;
+	}
+	/**
+	 * @param stageID the stageID to set
+	 */
+	public void setStageID(int stageID) {
+		this.stageID = stageID;
+	}
+	/**
+	 * @return the isArchived
+	 */
+	public boolean isArchived() {
+		return isArchived;
+	}
+	/**
+	 * @param isArchived the isArchived to set
+	 */
+	public void setArchived(boolean isArchived) {
+		this.isArchived = isArchived;
+	}
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	/**
+	 * @return the catColor
+	 */
+	public Color getCatColor() {
+		return CatColor;
+	}
+	/**
+	 * @param catColor the catColor to set
+	 */
+	public void setCatColor(Color catColor) {
+		CatColor = catColor;
+	}
+	
+	public String getCatColorString() {
+		
+	}
+	
+	
+	/**
+	 * @return the activities
+	 */
+	public ActivityListModel getActivities() {
+		return activities;
+	}
+	/**
+	 * @param activities the activities to set
+	 */
+	public void setActivities(ActivityListModel activities) {
+		this.activities = activities;
+	}
+	
+
+	
 
 }
